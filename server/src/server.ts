d00710929026f0b4ad102eaceedb0823e4295a94
Artur Aquino <arturbr@gmail.com>
2020-08-05 12:27:15 -0300

    Playing around REST methods to test everything

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,19 @@
-console.log('Hello World');
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.post('/users', (request, response) => {
+  console.log(request.body);
+
+  const users = [
+    { name: 'Diego', age: 25},
+    { name: 'Diego', age: 25},
+    { name: 'Diego', age: 25},
+  ];
+
+  return response.json(users);
+});
+
+app.listen(3333);
