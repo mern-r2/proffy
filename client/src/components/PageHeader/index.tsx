dc1d4f75ffeff38d4ecb45e65211de469a30637c
Artur Aquino <arturbr@gmail.com>
2020-08-06 17:14:40 -0300

    Fix formatting

--- client/src/components/PageHeader/index.tsx
+++ client/src/components/PageHeader/index.tsx
@@ -1,34 +1,32 @@
-import React from 'react';
+import React from "react";
 
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-import logoImg from '../../assets/images/logo.svg';
-import backIcon from '../../assets/images/icons/back.svg';
+import logoImg from "../../assets/images/logo.svg";
+import backIcon from "../../assets/images/icons/back.svg";
 
-import './style.css';
+import "./style.css";
 
 interface pageHeaderProps {
-    title: string;
+  title: string;
 }
 
 const PageHeader: React.FC<pageHeaderProps> = (props) => {
-    return (
-        <header className="page-header">
-            <div className="top-bar-container">
-                <Link to="/">
-                    <img src={backIcon} alt="voltar" />
-                </Link>
-                <img src={logoImg} alt="Proffy" />
-            </div>
+  return (
+    <header className="page-header">
+      <div className="top-bar-container">
+        <Link to="/">
+          <img src={backIcon} alt="voltar" />
+        </Link>
+        <img src={logoImg} alt="Proffy" />
+      </div>
 
-            <div className="header-content">
-                <strong>{props.title}</strong>
-                {props.children}
-            </div>
-
-           
-        </header>
-    );
-}
+      <div className="header-content">
+        <strong>{props.title}</strong>
+        {props.children}
+      </div>
+    </header>
+  );
+};
 
 export default PageHeader;
