a52fc94359985f9c3d8c289aa5f9d4c398ae8896
Artur Aquino <arturbr@gmail.com>
2020-08-07 13:11:46 -0300

    TextArea component (used in bio)

--- client/src/pages/TeacherForm/index.tsx
+++ client/src/pages/TeacherForm/index.tsx
@@ -5,6 +5,7 @@ import Input from "../../components/Input";
 import warningIcon from "../../assets/images/icons/warning.svg";
 
 import "./style.css";
+import Textarea from "../../components/TextArea";
 
 function TeacherForm() {
   return (
@@ -20,6 +21,7 @@ function TeacherForm() {
           <Input name="name" label="Nome Completo" />
           <Input name="avatar" label="Avatar" />
           <Input name="whatsapp" label="WhatsApp" />
+          <Textarea name="bio" label="Biografia" />
         </fieldset>
 
         <fieldset>
