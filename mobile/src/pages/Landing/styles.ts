4cbb354e35167e332ab823804ee12ea883abcea5
Artur Aquino <arturbr@gmail.com>
2020-08-09 18:47:20 -0300

    Landing page

--- mobile/src/pages/Landing/styles.ts
+++ mobile/src/pages/Landing/styles.ts
@@ -9,20 +9,58 @@
    },
 
    banner: {
-     width: 100%,
+     width: '100%',
      resizeMode: 'contain',
    },
 
    title: {
-     color: '#FFF',
+     fontFamily: 'Poppins_400Regular',
      fontSize: 20,
      lineHeight: 30,
      marginTop: 80,
    },
 
    titleBold: {
-     fontWeight: 'bold',
-   }
+     fontFamily: 'Poppins_600SemiBold',
+   },
+
+   buttonsContainer: {
+     flexDirection: 'row',
+     marginTop: 40,
+     justifyContent: 'space-between',
+   },
+
+   button: {
+     height: 150,
+     width: '48%',
+     backgroundColor: '#333',
+     borderRadius: 8,
+     padding: 24,
+     justifyContent: 'space-between',
+   },
+
+   buttonPrimary: {
+     backgroundColor: '#9871f5',
+   },
+
+   buttonSecondary: {
+     backgroundColor: '#04d361',
+   },
+
+   buttonText: {
+     fontFamily: 'Archivo_700Bold',
+     color: '#FFF',
+     fontSize: 20,
+   },
+
+   totalConnections: {
+     fontFamily: 'Poppins_400Regular',
+     color: '#d4c2ff',
+     fontSize: 12,
+     lineHeight: 20,
+     maxWidth: 140,
+     marginTop: 40,
+   },
  });
 
  export default styles;
